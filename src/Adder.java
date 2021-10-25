8330830345650e362b5a1cec43fae403950620c4
Tshepang <radebetshepang64@gmail.com>
2021-10-25 04:16:43 +0200

    Abstract

--- src/Adder.java
+++
@@ -1,6 +0,0 @@
-public class Adder {
-
-    public static void main(String[] args) {
-
-    }
-}
--- src/Arithmetic.java
+++ src/Arithmetic.java
@@ -1,11 +1,23 @@
 public class Arithmetic {
 
+    private int a;
+    private int b;
+
     public int add(int a , int b)
     {
         return a + b;
     }
-    class Adder extends Arithmetic {
-    }
-
 
 }
+
+class Adder extends Arithmetic {
+}
+
+class Test{
+
+    public static void main(String[] args) {
+        Arithmetic d = new Arithmetic();
+        System.out.println(d.add(2 , 3));
+    }
+}
+
